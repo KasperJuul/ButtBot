49952525a0cd10ba110ff2d4407b83fad038c681
Kasper Juul <kasjuul@gmail.com>
2017-03-03 11:38:18 +0100

    ResourceManager
    Added Classes for resource manager

---
+++ FriendlyCitizen/Source/ResourceManager.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <BWAPI.h>
+#include <BWTA.h>
+
+
+// Remember not to use "Broodwar" in any global class constructor!
+
+class ResourceManager
+{
+public:
+	static void onFrame();
+
+};
+
