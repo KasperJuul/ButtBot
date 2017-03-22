aebe3a668ab6bfd24ad67d64bd85c4bad30521ff
yuirick <yuiricklive@gmail.com>
2017-03-22 14:16:22 +0100

    I'm sorry

--- FriendlyCitizen/Source/FriendlyCitizen.h
+++ FriendlyCitizen/Source/FriendlyCitizen.h
@@ -1,9 +1,6 @@
 #pragma once
 #include <BWAPI.h>
 #include <BWTA.h>
-#include <windows.h>
-
-DWORD WINAPI AnalyzeThread();
 
 // Remember not to use "Broodwar" in any global class constructor!
 
@@ -29,7 +26,5 @@ public:
 	virtual void onSaveGame(std::string gameName);
 	virtual void onUnitComplete(BWAPI::Unit unit);
 	// Everything below this line is safe to modify.
-	
-	static std::string minelog;
-	void drawTerrainData();
+
 };
