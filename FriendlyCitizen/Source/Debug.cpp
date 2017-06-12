2ecf911077ccde50e266b10fc6a76f026b086601
KasperJuul <kasjuul@gmail.com>
2017-06-12 11:23:08 +0200

    Scout and resource manager (#17)
    A working version of scout and resource manager is now implemented.

--- FriendlyCitizen/Source/Debug.cpp
+++ FriendlyCitizen/Source/Debug.cpp
@@ -47,10 +47,16 @@ void Debug::writeTimedLog(std::string message, std::string fileName, std::string
 	writeTo.close();
 }
 
-void Debug::screenInfo(){//More complex debugging can be done here in the future.
+void Debug::screenInfo(){
+	for (auto w : InformationManager::workerUnits){
+		Broodwar->drawTextMap(w->unit->getPosition(), "%c%d", Text::Yellow, w->unit->getID());
+		Broodwar->drawTextMap(w->unit->getPosition() + Position(0, -10), "%c%d", Text::Cyan, w->state);
+	}
+
 	for (auto &u : Broodwar->self()->getUnits()){
 		if (u->getType().isWorker()){
-			Broodwar->drawTextMap(u->getPosition(), "%c%d", Text::Blue, u->getID());
+			continue;
+			//Broodwar->drawTextMap(u->getPosition(), "%c%d", Text::Yellow, u->getID());
 		}
 		else if (u->getType().isResourceDepot()){
 			Broodwar->drawTextMap(u->getPosition() + Position(0,-15), "%c%d", Text::Orange, u->getID());
@@ -66,12 +72,34 @@ void Debug::screenInfo(){//More complex debugging can be done here in the future
 		}
 	}
 
-	std::string b = "Centers: " + std::to_string(InformationManager::centers.size());
-	std::string w = "Workers: " + std::to_string(InformationManager::centers.at(0).wrkUnits.size());
+	std::string centers = "Centers: " + std::to_string(InformationManager::centers.size());
+	std::string workers = "Workers: " + std::to_string(InformationManager::workerUnits.size());
 	
-	Broodwar->drawTextScreen(20, 0, b.c_str());
-	Broodwar->drawTextScreen(20, 10, w.c_str());
+	Broodwar->drawTextScreen(20, 0, centers.c_str());
+	Broodwar->drawTextScreen(20, 10, workers.c_str());
 	Broodwar->drawTextScreen(20, 30, BuildingPlanner::plan.c_str());
+
+	int some = 0;
+	for (auto m :ResourceManager::minPatches){
+		std::string minstring = m.name + ": ";
+		std::string gathered = "";
+		if (!m.workers.empty()){
+			for (unsigned int j = 0; j < m.workers.size(); j++){
+				minstring += "[" + std::to_string(m.workers.at(j)->getID()) + "] ";
+			}
+		}
+		if (m.unit->isBeingGathered()){
+			gathered = "True";
+		}
+		else {
+			gathered = "False";
+		}
+		Broodwar->drawTextScreen(20, 40 + (some * 10), minstring.c_str());
+		Broodwar->drawTextMap(m.unit->getPosition() + Position(-5, -10), "%c%s", Text::Yellow, m.name.c_str());
+		Broodwar->drawTextMap(m.unit->getPosition() + Position(-5, -20), "%c%s", Text::Yellow, gathered.c_str());
+
+		some++;
+	}
 
 	Broodwar->drawTextScreen(588, 15, "%c%d/%d", Text::Yellow, Broodwar->self()->supplyUsed() / 2, BWAPI::Broodwar->self()->supplyTotal() / 2);
 	Broodwar->drawTextScreen(452, 15, "%c%d", Text::Yellow, InformationManager::reservedMinerals);
--- FriendlyCitizen/Source/IntelManager.cpp
+++ FriendlyCitizen/Source/IntelManager.cpp
@@ -4,88 +4,115 @@
 
 bool IntelManager::highX = false;
 bool IntelManager::highY = false;
-UnitStatus IntelManager::scout;
+//UnitStatus IntelManager::scout;
 BWAPI::Unit IntelManager::scouter;
 bool IntelManager::scouting = false;
 int IntelManager::initBaseCount;
 std::set<BWTA::BaseLocation*> IntelManager::remainingLocations;
+WorkerUnit* IntelManager::scout;
+BWTA::BaseLocation* IntelManager::gotoLocation;
+BWTA::BaseLocation* IntelManager::enemyBaselocation;
+BWTA::Polygon IntelManager::enemyRegionCircle;
+bool IntelManager::goingToLocation;
+bool IntelManager::firstpos;
 
-void IntelManager::StartScouting()
-{
-	Unit scoutUnit;
-	for (auto u : BWAPI::Broodwar->self()->getUnits()){
-		if (u->getType().isWorker()){
-			scoutUnit = u;
-			break;
+BWAPI::Position pos;
+int itr = 0;
+
+void IntelManager::hireScout(){
+	for (auto w : InformationManager::workerUnits){
+		if (!w->unit->isCarryingMinerals()){
+			scout = w;
+			for (unsigned int i = 0; i < scout->mineral->workers.size(); i++){
+				if (scout->mineral->workers.at(i) = scout->unit){
+					scout->mineral->workers.erase(scout->mineral->workers.begin() + i);
+					scout->inQ = false;
+					scout->returningCargo = false;
+					scout->state = 0;
+				}
+			}
 		}
 	}
-	for (auto u : InformationManager::ourUnits){
-		if (scoutUnit == u.self){
-			IntelManager::scout = u;
-		}
-	}
-	//Infomanager?
-	//InformationManager::ReadUnit(scout);
-	//If readunit returns UnitState::RedAlert or other vital states, don't use the unit. Otherwise, do.
-	//InformationManager::AssignUnit(scout,UnitState::Scout,OwnerProcess::Intel);
+}
+
+void IntelManager::onStart(){
 	IntelManager::remainingLocations = BWTA::getStartLocations();
 	IntelManager::remainingLocations.erase(BWTA::getStartLocation(BWAPI::Broodwar->self()));
-	IntelManager::initBaseCount = IntelManager::remainingLocations.size();
-	if (BWTA::getStartLocation(BWAPI::Broodwar->self())->getPosition().y > Broodwar->mapHeight()/2){
-		highY = true;
-	}
-	if (BWTA::getStartLocation(BWAPI::Broodwar->self())->getPosition().x > Broodwar->mapWidth() / 2){
-		highX = true;
-	}
+	goingToLocation = false;
+
 
 }
 
-void IntelManager::ScoutOnFrame(){
-	if (!IntelManager::scouting){
-		if (Broodwar->self()->supplyUsed() > 10){//Temporary
-			if (IntelManager::scout.self->exists()){//TODO: Refine scout.
-				//if (IntelManager::scout.self->isIdle()){ 
-				//if (!IntelManager::scout.self->isCarryingMinerals() /*&& !IntelManager::scout.self->isGatheringMinerals()*/){
-					InformationManager::AssignUnit(scout, UnitState::WORKING, OwnerProcess::INTEL);
-					IntelManager::scouting = true;
-				//}
-			}
-			else {
-				Unit scoutUnit;
-				for (auto u : Broodwar->self()->getUnits()){
-					if (u->getType().isWorker()){
-						scoutUnit = u;
-						break;
-					}
-				}
-				for (auto u : InformationManager::ourUnits){
-					if (scoutUnit == u.self){
-						IntelManager::scout = u;
-					}
-				}
-			}
-		}
+void IntelManager::onFrame(){
+	if (scout == NULL){
+		hireScout();
+		return;
 	}
-	if (IntelManager::scouting && IntelManager::remainingLocations.size() > 0){
-		if (IntelManager::initBaseCount > 2){
-			//Clockwise scouting
-			for (auto u : IntelManager::remainingLocations){
-				IntelManager::scout.self->move(u->getPosition());
-				if (IntelManager::scout.self->getType().sightRange() / 2 > IntelManager::scout.self->getDistance(u->getPosition())){
-					IntelManager::remainingLocations.erase(u);
-				}
-				break;
-			}
+
+	switch (scout->state){
+	case 0 :	// Initial State
+		if (scout->unit->isCarryingGas() || scout->unit->isCarryingMinerals()){
+			scout->unit->returnCargo();
+			scout->state = 1;
 		}
 		else {
-			//Flippant scouting
-			for (auto u : IntelManager::remainingLocations){
-				IntelManager::scout.self->move(u->getPosition());
-				if (IntelManager::scout.self->getType().sightRange()/2 > IntelManager::scout.self->getDistance(u->getPosition())){
-					IntelManager::remainingLocations.erase(u);
+			scout->state = 2;
+		}
+		break;
+	case 1 :	// Return Cargo if you have some
+		if (!scout->unit->isCarryingGas() && !scout->unit->isCarryingMinerals()){
+			scout->state = 2;
+		}
+		break;
+	case 2:		// Go to a starting base location
+		if (!goingToLocation){
+			if (!remainingLocations.empty()){
+				int distance = 9000;
+				for (auto u : IntelManager::remainingLocations){
+					if (scout->unit->getDistance(u->getPosition()) < distance){
+						gotoLocation = u;
+					}
 				}
-				break;
+				scout->unit->move(gotoLocation->getPosition());
+				goingToLocation = true;
 			}
 		}
+		if (scout->unit->getDistance(gotoLocation->getPosition() + Position(64, 48)) < 192){
+			scout->unit->stop();
+			goingToLocation = false;
+			remainingLocations.erase(gotoLocation);
+			if (scout->unit->getClosestUnit(Filter::IsEnemy, 128)){
+				enemyBaselocation = gotoLocation;
+				enemyRegionCircle = enemyBaselocation->getRegion()->getPolygon();
+				pos = enemyRegionCircle.getNearestPoint(scout->unit->getPosition());
+				itr = 0;
+				for (auto p : enemyRegionCircle){
+					if (p == pos){
+						break;
+					}
+					itr++;
+				}
+				scout->state = 3;
+			}
+		}
+		break;
+	case 3:		// Attack something at enemybase and circle the region to kite/Harass
+		if (scout->unit->isAttacking()){
+			scout->unit->move(enemyRegionCircle.getNearestPoint(pos));
+			scout->state = 4;
+		}
+
+		break;
+	case 4:		// Circle the region to kite enemy units
+		itr = itr % (enemyRegionCircle.size() - 1);
+		if (scout->unit->getDistance(enemyRegionCircle.at(itr)) < 160 || !scout->unit->isMoving()){
+			itr++;
+			itr = itr % (enemyRegionCircle.size() - 1);
+			scout->unit->move(enemyRegionCircle.at(itr));
+		}
+		break;
+
 	}
+
+
 }
